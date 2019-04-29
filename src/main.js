ac235e04cf5bbd78a187915d31ec5a36df30a09b
소희철 <thrrkthrrk@gmail.com>
2019-04-29 22:21:31 +0900

    Add FrameScore component

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FrameScore from './components/scoreboard/FrameScore'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
+const vm = new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App,
+    'frame-score': FrameScore
+  },
   template: '<App/>'
 })
+vm.$mount('#app')
